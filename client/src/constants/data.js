69a745bd166708160af7e8c63fa55a373c69cb88
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-10-01 23:20:00 +0530

    first commit

---
+++ client/src/constants/data.js
@@ -0,0 +1,34 @@
+const filters = [
+  {
+    name: "All",
+    imageUrl:
+      "https://imgs.search.brave.com/y7q1Oza9lnxPQkvYOMJnyiTCdrEE9RVMVrZDtm7l1t4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvODI3/NTcwNDYvcGhvdG8v/Z2FyZGVuLXdpdGgt/Y2hhaXIuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPWtpSzdF/cUd4bVBaWEhmdkY1/WkhIZUVNUWJ5Z2J1/LTY0R0pxOGw4R3Bf/c009",
+  },
+  {
+    name: "Food",
+    imageUrl:
+      "https://imgs.search.brave.com/r-yCsNF8wU2vjaxB1jBqU_zywikaCZHJXtqBOAogECw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQ3/NDE5NDYyMi9waG90/by9pbWFnZS1vZi1n/b2xnYXBwYS1wdXJp/LWhvbGxvdy1jcmlz/cHktc3BoZXJlcy13/aXRoLWhvbGVzLWZp/bGxpbmctb2YtcG90/YXRvZXMtb25pb24t/cG9tZWdyYW5hdGUu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PVB1eUx2WjVZbmJz/bTVKYmJLOUF0TWdi/ZENkbTMwZ2pFckxH/WkhVLVBnazg9",
+  },
+  {
+    name: "Fitness",
+    imageUrl:
+      "https://imgs.search.brave.com/5X4yfIKM8uPRfcwIejmVZnSSTosLNHWFldgT1UNkano/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9oaWdoaW50ZW5z/aXR5LWludGVydmFs/LXRyYWluaW5nLWhp/aXQtd29ya291dC1t/b2Rlcm4tZ3ltXzEy/NDU3NjMtMTEwOTIy/LmpwZz9zaXplPTYy/NiZleHQ9anBn",
+  },
+  {
+    name: "Travel",
+    imageUrl:
+      "https://imgs.search.brave.com/VSSsQ3ntaKxrGgUQhS_4dVIdePsfxZo-r06rPoF04Rc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9yZWQtZm9ydC1p/bmRpYS1kZWxoaS1m/YW1vdXMtZm9ydHJl/c3Mtc3VubnktZGF5/LXZpZXctdmlld18x/MDQyNjI4LTUzOTcw/NS5qcGc_c2l6ZT02/MjYmZXh0PWpwZw",
+    },
+  {
+    name: "Movie",
+    imageUrl:
+      "https://imgs.search.brave.com/z-w6b3_wDxrg6-Xd9AzdACm-IGVeTlwpp0VzbVHktT8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/MzM1Mjg2Ni9waG90/by9mcmllbmRzLWVu/am95aW5nLWEtY29t/ZWR5LW1vdmllLWF0/LXRoZS1jaW5lbWEu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PWo5NUU0aG5zWFEy/T0V0TmZKeDljdDJ0/MWJvOG1Udll3VzJo/aWNaVllsOVE9",
+  },
+  {
+    name: "Education",
+    imageUrl:
+      "https://imgs.search.brave.com/ggU95fHLJL2bZ4voRid9saGzzzXdvX6N8AhYqIahWOo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvMzM1/NDk2Mjc2L3N0b2Nr/LXBob3RvLWVsZW1l/bnRhcnktc3R1ZGVu/dC1zdHVkeWluZy1h/dC1zY2hvb2w.jpeg",
+  },
+];
+
+export default filters;
